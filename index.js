c3f796dcf1b84a3a3ac811b0958d4e3468d26899
Andy Read <andyreadpnw@gmail.com>
2019-10-28 15:33:42 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+const messages = [];
+
+function writeCards(names, event) {
+  for (let i = 0; i < names.length; i++) {
+    messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+  }
+
+  return messages;
+}
+
+function countDown(num){
+    let i = 0;
+    while (i < 11) {
+    console.log(i++);
+    }
+  }
