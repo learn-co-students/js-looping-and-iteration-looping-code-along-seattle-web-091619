c85961cb10f9397b9bb52e27f2521addde3a3a69
Leoncodes00 <chachaiyang3@gmail.com>
2019-10-28 17:23:32 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+
+
+let nameArray = []
+let suprise = "for the wonderful surprise gift!"
+
+function writeCards(array, suprises){
+    for(let i = 0; i < array.length; i++){
+        let emptyNames = `Thank you, ${array[i]}, ${suprise}`
+        nameArray.push(emptyNames)
+    }
+    return nameArray
+}
+
+function countDown(number){
+    let i = number
+    while(i > -1){
+        console.log(i)
+        i--
+    }
+}
