4f8f7ef00148f7c9f108ae342c13ebde32e17449
Soumya_Goyal <somyagoyal739@gmail.com>
2019-11-08 12:09:22 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+let greetings = []
+function writeCards(array, eventName){
+    for (let i = 0; i < array.length; i++){
+        greetings.push(`Thank you, ${array[i]}, for the wonderful surprise gift!`);
+    }
+    return greetings
+}
+console.log("hellllllooooo")
+
+function countDown(number){
+    let count = number
+    console.log(count)
+    while (count !== 0){
+        count--
+        console.log(count)
+    }
+}
+
+countDown(5)
